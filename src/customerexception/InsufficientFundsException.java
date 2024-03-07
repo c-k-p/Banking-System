f759c6ed058488fca13dd9915ccd87ae6a7d5574
Chandan Kumar Pandey <161229157+c-k-p@users.noreply.github.com>
2024-03-07 16:35:05 +0530

    Add files via upload
    pojo Package:
    
    BankAccountDetails: Represents bank account details (account number, holder name, balance).
    spec Package:
    
    BankAccount: Interface for banking operations (init, deposit, withdraw, balance, transfer).
    blogic Package:
    
    HDFCBankAccount, ICICIBankAccount, SBIBankAccount: Concrete bank account implementations.
    customerexception Package:
    
    InvalidAmountException: Custom exception for invalid amounts.
    InsufficientFundsException: Custom exception for insufficient funds.
    user Package:
    
    Bank: Main class for the banking system, facilitates user interactions.
    Key Concepts and Best Practices:
    
    Multiple Type Inheritance: BankAccount interface enables multiple bank implementations.
    High Cohesion: Classes focus on specific banking responsibilities.
    Strong Encapsulation: Private variables, controlled access with getters/setters.
    Loose Coupling: Interaction through the BankAccount interface reduces dependencies.
    Runtime Polymorphism: Polymorphic behavior in transferMoney method.
    Extensible and Scalable: Easy addition of new bank implementations.
    Dynamic Class Loading: Bank class allows dynamic loading and instantiation of bank accounts.

---
+++ src/customerexception/InsufficientFundsException.java
@@ -0,0 +1,17 @@
+package customerexception;
+
+
+
+public class InsufficientFundsException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public InsufficientFundsException() {
+		super();
+	}
+	
+	public InsufficientFundsException(String errMessage) {
+		super(errMessage);
+	}
+	
+}
---
+++ src/customerexception/InvalidAmountException.java
@@ -0,0 +1,17 @@
+package customerexception;
+
+
+
+public class InvalidAmountException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public InvalidAmountException() {
+		super();
+	}
+	
+	public InvalidAmountException(String errMessage) {
+		super(errMessage);
+	}
+	
+}
